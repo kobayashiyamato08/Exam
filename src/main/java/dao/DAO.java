3b35c77228a421c09fbae10544972fb37db2d23d
admin <admin@LAPTOP-KNBOTSVU>
2024-04-12 10:28:42 +0900

    DAOファイルを編集しました

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -1,5 +1,20 @@
 package dao;
 
-public class DAO {
+import java.sql.Connection;
 
+import jakarta.naming.InitialContext;
+import jakarta.sql.DataSource;
+
+public class DAO {
+	static DataSource ds;
+
+	// データベースの接続に必要な処理をgetConnectionというメソッドにまとめておく
+	public Connection getConnection() throws Exception {
+		if (ds==null) {
+			InitialContext ic=new InitialContext();
+			// score_manage_systemにアクセス
+			ds=(DataSource)ic.lookup("java:/comp/env/jdbc/score_manage_system");
+		}
+		return ds.getConnection();
+	}
 }
