e2deba77729626ba9c2fcbf429a5c6b37000fc5a
y_kobayashi <y_kobayashi@LAPTOP-O59QTU9D>
2024-04-12 12:44:34 +0900

    Revert "beanの作成"
    This reverts commit 07d942922227ff42a77e06ae993e6f7d56542651.

--- src/main/java/bean/School.java
+++ src/main/java/bean/School.java
@@ -1,21 +1,5 @@
 package bean;
 
-public class School implements java.io.Serializable {
+public class School {
 
-	private String cd;
-	private String name;
-
-	public String getCd() {
-		return cd;
-	}
-	public String getName() {
-		return name;
-	}
-	
-	public void setCd(String cd) {
-		this.cd=cd;
-	}
-	public void setName(String name) {
-		this.name=name;
-	}
 }
