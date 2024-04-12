32032944940b44d92e8a64ed1c656e6c6abeda4e
admin <admin@LAPTOP-KNBOTSVU>
2024-04-12 14:31:30 +0900

    schoolとteacherのbeanファイルの作成

--- src/main/java/bean/School.java
+++ src/main/java/bean/School.java
@@ -1,5 +1,23 @@
 package bean;
 
-public class School {
-
+public class School implements java.io.Serializable {
+	
+	private String cd;
+	private String name;
+	
+	public String getCd() {
+		return cd;
+	}
+	
+	public String getNmae() {
+		return name;
+	}
+	
+	public void setCd() {
+		this.cd = cd;
+	}
+	
+	public void setName() {
+		this.name = name;
+	}
 }
