58cdf5367d1b798fba334729d69db22e85c173e5
y_kobayashi <y_kobayashi@LAPTOP-O59QTU9D>
2024-04-15 11:18:29 +0900

    beanの作成

--- src/main/java/bean/School.java
+++ src/main/java/bean/School.java
@@ -1,7 +1,6 @@
 package bean;
 
 public class School implements java.io.Serializable {
-<<<<<<< HEAD
 
 	private String cd;
 	private String name;
@@ -21,25 +20,3 @@ public class School implements java.io.Serializable {
 		this.name=name;
 	}
 }
-=======
-	
-	private String cd;
-	private String name;
-	
-	public String getCd() {
-		return cd;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setCd() {
-		this.cd = cd;
-	}
-	
-	public void setName() {
-		this.name = name;
-	}
-}
->>>>>>> branch 'master' of https://github.com/kobayashiyamato08/Exam.git
