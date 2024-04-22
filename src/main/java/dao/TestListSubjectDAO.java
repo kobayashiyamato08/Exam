3fc9489d80f00ac610f0a7be5eca3d70b1caed9e
y_kobayashi <y_kobayashi@LAPTOP-O59QTU9D>
2024-04-22 11:37:39 +0900

    a

--- src/main/java/dao/TestListSubjectDAO.java
+++ src/main/java/dao/TestListSubjectDAO.java
@@ -1,9 +1,71 @@
 package dao;
-
-import java.sql.*;
-import java.util.*;
-import bean.TestListSubject;
-
-public class TestListSubjectDAO {
 	
-}
+	import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.School;
+import bean.Student;
+import bean.Subject;
+
+	public class TestListSubjectDAO {
+		private static final String URL = "jdbc:h2:tcp://localhost/~/score_manage_system";
+		private Connection connection;
+		private String baseSql;
+		
+		public TestListSubjectDAO(String basesql, String baseSql) {
+			this.baseSql = baseSql;
+		}
+		
+		protected Connection getConnection() throws SQLException {
+	        if (connection == null || connection.isClosed()) {
+	            try {
+	                Class.forName("com.H2.jdbc.Driver");
+	                connection = DriverManager.getConnection(URL);
+	            } catch (ClassNotFoundException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	        return connection;
+	    }
+
+	    // データベースの接続を閉じるメソッド
+	    protected void closeConnection() throws SQLException {
+	        if (connection != null && !connection.isClosed()) {
+	            connection.close();
+	        }
+	    }
+
+	    // baseSqlのゲッターメソッド
+	    protected String getBaseSql() {
+	        return baseSql;
+	    }
+
+	    // baseSqlのセッターメソッド
+	    protected void setBaseSql(String baseSql) {
+	        this.baseSql = baseSql;
+	    }
+		
+		public List<Student> postFilter(ResultSet rSet) {
+			List<Student> filteredStudents = new ArrayList<>();
+		    try {
+		        while (rSet.next()) {
+		        }
+		    } catch (SQLException e) {
+		        e.printStackTrace();
+		    }
+		    return filteredStudents;
+		}
+		
+		public List<Student> filter(int entYear, String classNum, Subject subject, School school) {
+	   	 List<Student> filteredStudents = new ArrayList<>();
+	   	    return filteredStudents;
+	   }
+	}
+		
+		
+
+
