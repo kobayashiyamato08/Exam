9cacc0df8d724e52dc0a848e501c85ee9fe46ce9
y_kobayashi <y_kobayashi@LAPTOP-O59QTU9D>
2024-04-23 10:44:27 +0900

    Merge branch 'master' of https://github.com/kobayashiyamato08/Exam.git

--- src/main/java/dao/ClassNumDAO.java
+++ src/main/java/dao/ClassNumDAO.java
@@ -1,5 +1,13 @@
 package dao;
 
-public class ClassNumDAO {
+import java.util.ArrayList;
+import java.util.List;
 
+import bean.School;
+
+public class ClassNumDAO extends DAO {
+	public List<String> filter(School school) {
+	   	 List<String> filteredStudents = new ArrayList<>();
+	   	    return filteredStudents;
+	   }
 }
