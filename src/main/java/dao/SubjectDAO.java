93cbb0a1184769e94e7d690cb64d800f4fff6cf2
y_kobayashi <y_kobayashi@LAPTOP-O59QTU9D>
2024-04-23 10:59:39 +0900

    a

--- src/main/java/dao/SubjectDAO.java
+++ src/main/java/dao/SubjectDAO.java
@@ -3,45 +3,42 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
 
 import bean.School;
 import bean.Subject;
 
 public class SubjectDAO extends DAO {
-	public Subject get(String cd, School school)
-		throws Exception {
-		Subject subject=null;
-		
-		Connection con=getConnection();
-		
-		PreparedStatement st;
-		st=con.prepareStatement(
-				"select * from subject where cd=? and school=?");
-		st.setString(1, cd);
-		st.setSchool(2, school);
-		ResultSet rs=st.executeQuery();
-		
-		if (rs.next()) {
-			subject = new Subject();
-			subject.setCd(rs.getString("cd"));
-			subject.setSchool(rs.getSchool("school"));
-			
-		}
-		return subject;
+	public Subject get(String cd, School school) throws Exception {
+	    Subject subject = null;
+	    
+	    Connection con = getConnection();
+	    PreparedStatement st = null;
+	    ResultSet rs = null;
+	    
+	    try {
+	        st = con.prepareStatement("SELECT * FROM subject WHERE cd=? AND school=?");
+	        st.setString(1, cd);
+	        st.setString(2, school.getName()); // 仮に学校名を取得するメソッドgetName()を使用するとします
+	        rs = st.executeQuery();
+	        
+	        if (rs.next()) {
+	            subject = new Subject();
+	            subject.setCd(rs.getString("cd"));
+	            subject.setSchool(school); // 学校オブジェクトをそのまま設定します
+	            // 他の属性も必要に応じて取得し、設定します
+	        }
+	    } finally {
+	        if (rs != null) {
+	            rs.close();
+	        }
+	        if (st != null) {
+	            st.close();
+	        }
+	        if (con != null) {
+	            con.close();
+	        }
+	    }
+	    
+	    return subject;
 	}
-	
-	public List<Subject> filter(ResultSet School) {
-		List<Subject> filteredSubjects =new ArrayList<>();
-		return filteredSubjects;
-}
-	public boolean save(Subject subject) {
-		return true;
-	}
-	public boolean delete(Subject subject) {
-		return true;
-	}
-	
-	
 }
