0806e95090442cecd937ba862426d89ae048b336
y_kobayashi <y_kobayashi@LAPTOP-O59QTU9D>
2024-04-26 10:03:56 +0900

    a

--- src/main/java/dao/SchoolDAO.java
+++ src/main/java/dao/SchoolDAO.java
@@ -1,5 +1,30 @@
 package dao;
 
-public class SchoolDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import bean.School;
+
+public class SchoolDAO extends DAO {
+public String BaseSql;
+	
+	public School get(String no)
+			throws Exception {
+			School school=null;
+			
+			Connection con=getConnection();
+			
+			PreparedStatement st;
+			st=con.prepareStatement(
+					"select * from student where no=?");
+				st.setString(1, no);
+				ResultSet rs=st.executeQuery();
+				if (rs.next()) {
+					
+					school = new School();
+					school.setCd(rs.getString("no"));
+				}
+				return school;
+		}
 }
